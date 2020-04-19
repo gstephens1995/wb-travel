bbbf6d1d40b43faf979b5521349ff6d39aaea22f
thiri3377 <thirihsumyataung@aol.com>
2020-04-18 21:41:22 -0400

    refactor: unnecessary www removed and .gitignore added

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -17,4 +15,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+var port = process.env.port || 3000;
+http.createServer(app).listen(port);
