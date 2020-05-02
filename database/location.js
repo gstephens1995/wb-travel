12c1d8f43dbdcb5f4c003e0359f4c0966656d82d
Prince-J <55772664+Prince-J@users.noreply.github.com>
2020-05-01 23:32:05 -0400

    Update location.js

--- database/location.js
+++ database/location.js
@@ -1,19 +1,17 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-function createAcct(userName, email, password) {
-    MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+        throw err;
+    }
+
+    var dbo = db.db("data");
+
+    dbo.createCollection("locations", function (err, res) {
         if (err) {
             throw err;
         }
-
-        var dbo = db.db("data");
-
-        dbo.createCollection("locations", function (err, res) {
-            if (err) {
-                throw err;
-            }
-            db.close();
-        });
+        db.close();
     });
-}
+});
